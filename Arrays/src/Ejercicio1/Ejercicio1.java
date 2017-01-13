9b91d06212f5039e535720b08ca4200d81ba62f3
david <david.palanco.daw@gmail.com>
2017-01-13 11:16:09 +0100

    añadir leer teclado

--- Arrays/src/Ejercicio1/Ejercicio1.java
+++ Arrays/src/Ejercicio1/Ejercicio1.java
@@ -5,9 +5,30 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		int[] array1 = new int [10];
 		
+		//crear
+		for (int i = 0; i < array1.length; i++) {
+			array1[i]=i*2;
+		}
+		/*
+		//visualizar
+		for (int i = 0; i < array1.length; i++) {
+			System.out.println(array1[i]+ " ");
+		}*/
+		//Es mejor con metodos
+		visualizarArray(array1);
+		
+		
+		
 
+      
 
+	}
 
+	private static void visualizarArray(int[] array1) {
+		for (int i = 0; i < array1.length; i++) {
+			System.out.println(array1[i]+ " ");
+		}
+		
 	}
 
 }
