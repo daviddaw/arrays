5012183ea8e7ad37a20226fa564fa3aaa645e395
david <david.palanco.daw@gmail.com>
2017-01-19 12:44:10 +0100

    truco

---
+++ Arrays/src/Ejercicio1/Ejercicio3.java
@@ -0,0 +1,17 @@
+package Ejercicio1;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+/*
+ * pedir numero validando entre 0 y 10
+ * si esta entre cero y  10
+ * meteras el numero en la posicion
+ * su metes 5 numeros 6
+ * al preguntarlo t e saldra
+ * */
