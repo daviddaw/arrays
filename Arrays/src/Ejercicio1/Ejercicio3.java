18f4175990b3e6ce4100641baaeb5db05de7bc3b
david <david.palanco.daw@gmail.com>
2017-01-19 12:50:59 +0100

    ejer 3 inicio

--- Arrays/src/Ejercicio1/Ejercicio3.java
+++ Arrays/src/Ejercicio1/Ejercicio3.java
@@ -3,7 +3,13 @@ package Ejercicio1;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("introduce un numero");
+		nuemero=LeerTeclado.readInteger():
+		while (n>=0 && n<=10) 
+		{
+			
+			
+		}
 
 	}
 
