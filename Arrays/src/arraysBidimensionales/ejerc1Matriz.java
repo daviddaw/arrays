7d5f170d1f37f00966ec3b6e1c5757876506ab78
david <david.palanco.daw@gmail.com>
2017-02-03 11:58:09 +0100

    ejercicio 1 completado

--- Arrays/src/arraysBidimensionales/ejerc1Matriz.java
+++ Arrays/src/arraysBidimensionales/ejerc1Matriz.java
@@ -4,8 +4,23 @@ public class ejerc1Matriz {
 
 	public static void main(String[] args) {
 	
+		int matriz[][]= new int [5][5];
 		
-
+		for (int i=0; i<matriz.length;i++ ){
+			for(int j=0;j<matriz[i].length;j++){
+				matriz[i][j]=i+j;
+				
+			}
+			
+		}
+		
+		for (int i=0; i<matriz.length;i++ ){
+			System.out.println();//fijarse
+			for(int j=0;j<matriz[i].length;j++){
+				System.out.print(matriz[i][j]+" ");
+			}
+		}
+		
 	}
 
 }
---
+++ Arrays/src/arraysBidimensionales/ejerc2matriz.java
@@ -0,0 +1,10 @@
+package arraysBidimensionales;
+
+public class ejerc2matriz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
