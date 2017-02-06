eee59434bcce40fd41ea6a465b3a9a9523175f8c
daviddaw <kilimanjaro_10>
2017-02-06 20:44:53 +0100

    manual

--- Arrays/src/Simples/Ejercicio01.java
+++ Arrays/src/Simples/Ejercicio01.java
@@ -2,23 +2,27 @@ package Simples;
 
 public class Ejercicio01 {
 
-	public static void main(String[] args) {////////////
+	public static void main(String[] args) {
+		//Declaracion de dos arrays de 10 numeros en el Main
 		int[] array1 = new int [10];
 		int[] array2 = new int [10];
 		
+		//crear arrays desde teclado
 		crearArray(array1);
 		crearArray(array2);
+		
+		
 		visualizarArray(array1);
 		visualizarArray(array2);
+		//visualizar Arrays //Multiplicar Arrays
 		visualizarArray(productoArray(array1, array2));
 	}
 	
 	
 	public static void crearArray (int[] array)	{
-	
+	//Cargar arrays desde teclado
 		for (int i = 0; i < array.length; i++) 
 		{
-			
 			System.out.println("Faltan por introducir "+(10-i)+" numeros");
 			array[i]=LeerTeclado.readInteger();
 		}
@@ -26,7 +30,7 @@ public class Ejercicio01 {
 	}
 
 	public static int [] productoArray(int[] array1, int[] array2)
-	/*no olvidar poner corchetes "public static int []" */
+	//Multiplicar Arrays
 	{
 		int[] producto=new int[array1.length];
 		for (int i = 0; i < array1.length; i++) {
