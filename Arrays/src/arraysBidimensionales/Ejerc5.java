d5a83206b0cf31e3490e2692663c8908ee29e73d
david <david.palanco.daw@gmail.com>
2017-02-09 12:46:19 +0100

    ppp

---
+++ Arrays/src/arraysBidimensionales/Ejerc5.java
@@ -0,0 +1,62 @@
+package arraysBidimensionales;
+
+public class Ejerc5 {
+
+	public static void main(String[] args) {
+		int matriz[][]= new int [30][5];
+		int Alumnos;
+		int Notas;
+
+		for (int i=0; i<matriz.length;i++ ){
+			for(int j=0;j<matriz[i].length;j++){
+				matriz[i][j]=(int)(Math.random() * 9 +1) ;
+				//matriz[i][j]=i+j; ESto solo sumaria posiciones
+
+
+			}
+
+		}
+
+
+
+
+		//solo imprime
+		int max;
+		double media=0,suma=0;
+		max=Integer.MIN_VALUE;
+
+		for (int i=0; i<matriz.length;i++ )
+		{
+			
+			
+			
+			
+			
+			System.out.println();
+			suma=0;
+			for(int j=0;j<matriz[i].length;j++){
+				System.out.print(matriz[i][j]+" ");
+
+				suma=suma+matriz[i][j];
+				if (matriz[i][j]>max) 
+				{
+					max= matriz[i][j];
+				}
+				
+			}
+
+
+			media=suma/matriz[0].length;
+			System.out.println(" La nota media de cada alumno  es "+media);
+		}
+		System.out.println("La nota maxima es "+max );
+
+
+
+
+
+
+	}
+}
+
+
--- Arrays/src/arraysBidimensionales/Ejerc5Matriz.java
+++ Arrays/src/arraysBidimensionales/Ejerc5Matriz.java
@@ -3,54 +3,26 @@ package arraysBidimensionales;
 public class Ejerc5Matriz {
 
 	public static void main(String[] args) {
-		int matriz[][]= new int [30][5];
-		int Alumnos;
-		int Notas;
-
-		for (int i=0; i<matriz.length;i++ ){
-			for(int j=0;j<matriz[i].length;j++){
-			matriz[i][j]=(int)(Math.random() * 9 +1) ;
-			//matriz[i][j]=i+j; ESto solo sumaria posiciones
-				
-				
-			}
-			
-		}
 		
-		
+		rellenarMatriz(array1);
+		visualizarMatriz(array1);
+		operacionesFila(array1);
+		System.out.println();
+		operacionesColumna(array1);
 
+	}
 
-		//solo imprime
-		int max;
-		double media=0,suma=0;
-		max=Integer.MIN_VALUE;
-		
-		for (int i=0; i<matriz.length;i++ )
-		{
-			System.out.println();
-			suma=0;
-			for(int j=0;j<matriz[i].length;j++){
-				System.out.print(matriz[i][j]+" ");
-
-				suma=suma+matriz[i][j];
-				if (matriz[i][j]>max) 
-				{
-					max= matriz[i][j];
-				}		
-			}
-
-
-			media=suma/matriz[0].length;
-			System.out.println(" La media de la fila es "+media);
-		}
-		System.out.println("La nota maxima es "+max );
- 
-
-		
-
+public static void rellenarMatriz(int [][]array){
 	
-		
-	}
-	}
+}
+public static void visualizarMatriz(int [][]array){
+	
+}
+public static void operacionesFila(int [][]array){
+	
+}
+public static void operacionescolumna(int [][]array){
+	
+}
 
-
+}
