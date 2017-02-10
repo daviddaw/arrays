d40486e911543987adcd61dfc75741aeab577fa8
daviddaw <kilimanjaro_10>
2017-02-10 15:14:54 +0100

    jj

--- Arrays/src/arraysBidimensionales/E5.java
+++ Arrays/src/arraysBidimensionales/E5.java
@@ -9,69 +9,78 @@ public class E5 {
 		operacionesFila(array1);
 		System.out.println();
 		operacionesColumna(array1);
-		
-		
-		
-		
+		System.out.println();
+		operacionesTotales(array1);
+
+
+
 
 	}
 
-public static void rellenarAleatorio(int [][]array){
-	for (int i = 0; i < array.length; i++) {
-		for (int j = 0; j < array[i].length; j++) {
-			array[i][j]=(int)(Math.random()*9+1);
-		}
-	}
-}
-
-
-public static void visualizarMatriz(int [][]matriz){
-	int max=Integer.MIN_VALUE;
-	double media=0,suma=0;
-	for (int i = 0; i < matriz.length; i++) {
-		for (int j = 0; j < matriz[i].length; j++) {
-			System.out.print(" "+matriz[i][j]+" ");
-			suma=suma+matriz[i][j];
-			if (matriz[i][j]>max) 
-			{
-				max= matriz[i][j];
+	public static void rellenarAleatorio(int [][]array){
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j]=(int)(Math.random()*9+1);
 			}
 		}
-		System.out.println(" ");
 	}
-	System.out.println("La nota maxima es "+max );
-	media=suma/(matriz[0].length*matriz.length);
-	System.out.println(" La nota media de todos es "+media);
-}
 
-//fila
-public static void operacionesFila(int [][]matriz){
-	
-	double media=0,suma=0;
-	
-	System.out.println(" ");
-	for (int i = 0; i < matriz.length; i++) {
-		suma =0;
-		for (int j = 0; j < matriz[i].length; j++) {
-			suma=suma+matriz[i][j];
 
-		}
-		media=suma/matriz[0].length;
-		System.out.println(" La nota media de cada alumno  es "+media);
-	}
-	
-}
-//columna
-public static void operacionesColumna(int [][]matriz){
-	double media=0,suma=0;
-	for (int j = 0; j < matriz[j].length; j++) {
-		suma =0;
+	public static void visualizarMatriz(int [][]matriz){
+
 		for (int i = 0; i < matriz.length; i++) {
-			suma=suma+matriz[i][j];
-		}
-		media=suma/matriz[0].length;
-		System.out.println(" La nota media de cada asignatura  es "+media);
-	}
-}
+			for (int j = 0; j < matriz[i].length; j++) {
+				System.out.print(" "+matriz[i][j]+" ");
 
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+	//fila
+	public static void operacionesFila(int [][]matriz){
+		double media=0,suma=0;
+		System.out.println(" ");
+		for (int i = 0; i < matriz.length; i++) {
+			suma =0;
+			for (int j = 0; j < matriz[i].length; j++) {
+				suma=suma+matriz[i][j];
+
+			}
+			media=suma/matriz[0].length;
+			System.out.println(" La nota media de cada alumno  es "+media);
+		}
+
+	}
+	//columna
+	public static void operacionesColumna(int [][]matriz){
+		double media=0,suma=0;
+		for (int j = 0; j < matriz[j].length; j++) {
+			suma =0;
+			for (int i = 0; i < matriz.length; i++) {
+				suma=suma+matriz[i][j];
+			}
+			media=suma/matriz[0].length;
+			System.out.println(" La nota media de cada asignatura  es "+media);
+		}
+	}
+
+	public static void operacionesTotales(int [][]matriz){
+		int max=Integer.MIN_VALUE;
+		double media=0,suma=0;
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				suma=suma+matriz[i][j];
+				if (matriz[i][j]>max) 
+				{
+					max= matriz[i][j];
+				}
+			}
+
+		}
+		System.out.println(" La nota maxima es "+max );
+		media=suma/(matriz[0].length*matriz.length);
+		System.out.println(" La nota media de todos los alumnos con todas las asignaturas es  "+media);
+	}
 }
