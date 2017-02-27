cca6ce222abe2ef1858366873a53a6d2bdb74c96
david <david.palanco.daw@gmail.com>
2017-02-27 14:22:18 +0100

    yh

--- Arrays/src/ejercicios_Strings/Ejercicio1_String.java
+++ Arrays/src/ejercicios_Strings/Ejercicio1_String.java
@@ -5,15 +5,18 @@ package ejercicios_Strings;
 public class Ejercicio1_String {
 
 	public static void main(String[] args) {
-		// 1.	Dada una cadena introducida por teclado, 
-		//obtener su longitud, 
-		//el carácter que ocupa  el centro de la  cadena 
-		// y visualizar la mitad derecha y la mitad izquierda de la misma.
-		System.out.println("Escribe una palabra");
 		int numero;
 		String palabra;
+		// 1.	Dada una cadena introducida por teclado, 
+		System.out.println("Escribe una palabra");
 		palabra=LeerTeclado.readString();
+		//obtener su longitud,
+		System.out.println(palabra.length());
+		//el carácter que ocupa  el centro de la  cadena 
 		
+		System.out.println(palabra.charAt(palabra.length()/2));
+		// visualizar la mitad derecha y la mitad izquierda de la misma.
+        System.out.println(palabra.substring(0, palabra.length()/2)+" "+(palabra.substring(3,palabra.length())));
 		
 	}
 
