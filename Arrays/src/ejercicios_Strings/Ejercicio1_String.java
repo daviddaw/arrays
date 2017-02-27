3e29398abbab0ab0701a9145bb0efd82a6c2c0bd
david <david.palanco.daw@gmail.com>
2017-02-27 13:36:57 +0100

    g

---
+++ Arrays/src/ejercicios_Strings/Ejercicio1_String.java
@@ -0,0 +1,20 @@
+package ejercicios_Strings;
+
+
+
+public class Ejercicio1_String {
+
+	public static void main(String[] args) {
+		// 1.	Dada una cadena introducida por teclado, 
+		//obtener su longitud, 
+		//el carácter que ocupa  el centro de la  cadena 
+		// y visualizar la mitad derecha y la mitad izquierda de la misma.
+		System.out.println("Escribe una palabra");
+		int numero;
+		String palabra;
+		palabra=LeerTeclado.readString();
+		
+		
+	}
+
+}
