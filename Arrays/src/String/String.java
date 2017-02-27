89ea5b9a7449afdec4b78ed8b24a3a936fec56c1
david <david.palanco.daw@gmail.com>
2017-02-27 13:05:34 +0100

    k

--- Arrays/src/String/String.java
+++ Arrays/src/String/String.java
@@ -3,8 +3,10 @@ package String;
 public class String {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 1.	Dada una cadena introducida por teclado, obtener su longitud, el carácter que ocupa  el centro de la  cadena y visualizar la mitad derecha y la mitad izquierda de la misma.
 
+		
+		
 	}
 
 }
