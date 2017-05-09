fa13ddaf914a71d241fd8813ddcd5e0e739af385
david <david.palanco.daw@gmail.com>
2017-05-09 12:55:53 +0200

    d

--- Arrays/src/empleadosOrdenable/Algoritmos.java
+++ Arrays/src/empleadosOrdenable/Algoritmos.java
@@ -1,17 +1,51 @@
 package empleadosOrdenable;
 
-public class Algoritmos  implements Ordenable {
-	
-	
-   public static void  ordenar(Ordenable[] array) {
-}
-   public static Ordenable  maximo(Ordenable[] array) {
-	return null;
-}
-   public static Ordenable  minimo (Ordenable[] array) {
-	return null;
-}
-   
+public class Algoritmos  implements Ordenable{
 
+	public static void  ordenar(Ordenable[] array) {
+		Ordenable aux = array [0];
+		for (int i=0; i<array.length-1;i++)
+			for (int j=i+1;j<array.length;j++)
+				if(array[i].esMayor(array[j]))
+				{
+					aux=array[i];
+					array[i]=array[j];
+					array[j]=aux;
+				}
 	
+		System.out.println("");
+		//Imprime el array ordenado
+		System.out.println("El array ordenado es ");
+		for (int i = 0; i < array.length; i++) 
+		{//syso ("componente"+i);
+			
+			System.out.print(array[i]);
+		}
+		
+	}
+	
+	
+	public static Ordenable  maximo(Ordenable[] array) {
+		
+		
+		return null;
+	}
+	
+	
+	public static Ordenable  minimo (Ordenable[] array) {
+		return null;
+	}
+	
+	@Override
+	public boolean esMenor(Ordenable o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean esMayor(Ordenable o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
---
+++ Arrays/src/escepciones/Principal.java
@@ -0,0 +1,25 @@
+package escepciones;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		 int arrayEnterosE[]={1,2,3,0,7};
+		//try{ 
+		for (int i = 0; i < arrayEnterosE.length; i++) {
+			System.out.println(arrayEnterosE[i]);
+		
+		}
+		System.out.println(arrayEnterosE[7]);
+		
+//		}
+		
+		
+    
+                                                                                                                                                                                                                                                                                                                             
+    
+    
+
+	}
+
+}
