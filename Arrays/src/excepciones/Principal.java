2bd69873265b9a6dae0eee76fec32a589eaec00c
Daveto <David.palanco@gmail.com>
2017-05-09 13:16:19 +0200

    s

--- Arrays/src/excepciones/Principal.java
+++ Arrays/src/excepciones/Principal.java
@@ -1,18 +1,36 @@
-package escepciones;
+package excepciones;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
 		 int arrayEnterosE[]={1,2,3,0,7};
-		//try{ 
+		
+			
+	
 		for (int i = 0; i < arrayEnterosE.length; i++) {
-			System.out.println(arrayEnterosE[i]);
+			int resultado=0;
+			int numero=5;
+			 try{
+			resultado=numero/arrayEnterosE[i];
+			 }catch (Exception e){
+				 System.out.println("No se puede dividir entre cero");
+			 }
 		
 		}
-		System.out.println(arrayEnterosE[7]);
+		try {
+			System.out.println(arrayEnterosE[7]);
+		} catch (Exception e) {
+			System.out.println("Esa posicion no existe en el array");
+		} 
+		finally    {
+		System.out.println("fin de programa");
+		}
 		
-//		}
+		
+		
+		
+	
 		
 		
     
