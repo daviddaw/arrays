b9c6c353448a981366ab26430fd5aeae54f9df23
Daveto <David.palanco@gmail.com>
2017-05-16 23:39:44 +0200

    y

--- Arrays/src/excepciones3/EcuacionSegundoGrado.java
+++ Arrays/src/excepciones3/EcuacionSegundoGrado.java
@@ -1,12 +1,19 @@
 package excepciones3;
 
+
+
 public class EcuacionSegundoGrado {
 	private int a;
 	private int b;
 	private int c;
 	
-	
-	public void resolverEcuacion() throws Exception{
+	public EcuacionSegundoGrado(int a, int b, int c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public void solucion() throws Exception{
 		
 		int m,p,dividendo,discriminante;
 
@@ -22,24 +29,31 @@ public class EcuacionSegundoGrado {
 	
 
 		//ALGORITMO
+		
+		if (a==0 && b==0) {	
+			throw new Exception("Ecuacion degenerada");	
+		}
 		if (dividendo==0) {
 			System.out.println("No se puede dividir entre cero");
-
-
 		} else {
 			System.out.println();
 			if (discriminante<0) {//si es menor que cero, no tiene solucion. (5,3,4)
 				System.out.println("No tiene solucion, el discriminante es "+discriminante+" no se puede resolver la raiz de un número negativo.");
-
-			} else {
+				throw new Exception("Raices complejas");
+						
+		} else {
 				if (discriminante==0) {//si es cero tiene una solucion. (1,4,4)
 					System.out.println("Solo tiene una solucion ");
 					y1=(divisorP)/(dividendo);
+					
 					System.out.println(y1);
+					throw new Exception("La ecuación tiene una única raiz (x=c/b)");
 				}
 				else {
 					if (discriminante>0) {//si es mayor que cero tiene dos soluciones. (2,7,3)
 						System.out.println("Tiene dos soluciones");
+						
+						
 
 
 						y1=(-b+Math.sqrt(Math.pow(b, 2)-4*a*c))/(2*a);//importante los parentesis en multiplicaciones y divisiones
@@ -49,6 +63,7 @@ public class EcuacionSegundoGrado {
 
 						y2=(divisorN)/(dividendo);
 						System.out.println(+y2);
+						
 
 					}
 				}
@@ -89,9 +104,12 @@ public class EcuacionSegundoGrado {
 		this.c = c;
 	}
 
-	public EcuacionSegundoGrado(int a, int b, int c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
+
+
+	@Override
+	public String toString() {
+		return "EcuacionSegundoGrado [a=" + a + ", b=" + b + ", c=" + c + "]";
 	}
+
+
 }
---
+++ Arrays/src/excepciones3/Principal.java
@@ -0,0 +1,5 @@
+package excepciones3;
+
+public class Principal {
+EcuacionSegundoGrado name = new EcuacionSegundoGrado(4, 5, 7);
+}
