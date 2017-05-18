c3a74fde7b8f407e3b45d231b20e41ada00bd05d
Daveto <David.palanco@gmail.com>
2017-05-18 11:09:18 +0200

    h

---
+++ Arrays/src/colas/Tarjeta.java
@@ -0,0 +1,12 @@
+package colas;
+
+public class Tarjeta {
+	private String apellido;
+	private String nombre;
+	
+	public Tarjeta(String apellido, String nombre) {
+		this.apellido = apellido;
+		this.nombre = nombre;
+	}
+
+}
---
+++ Arrays/src/gasolinera/Surtidor.java
@@ -0,0 +1,5 @@
+package gasolinera;
+
+public class Surtidor {
+
+}
